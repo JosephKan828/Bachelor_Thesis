685cc8b8e5e9e1583578c9dbdf4b8e19d528f561
JosephKan828 <josephyck0828@gmail.com>
2025-02-10 11:15:03 +0800

    New LW/SW composite figure

--- Major/CCKWs_Climatology/CCKWs_lw_comp.py
+++ Major/CCKWs_Climatology/CCKWs_lw_comp.py
@@ -6,6 +6,8 @@ import netCDF4 as nc;
 import pickle as pkl;
 from matplotlib import pyplot as plt;
 from matplotlib.colors import TwoSlopeNorm;
+import matplotlib.gridspec as gridspec;
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes;
 
 # %% Section 2
 # Load file
@@ -61,6 +63,11 @@ for var in data_rm_cli.keys():
     data_sel[var] /= sel_lon.size;
 
 # # Daily averaging LW and SW 
+t_daily = np.array([
+    data_sel["t"][:, i*4:(i+1)*4].mean(axis=1)
+    for i in range(6)
+]).T;
+print(t_daily.shape)
 
 lw_daily = np.array([
     data_sel["lw"][:, i*4:(i+1)*4].mean(axis=1)
@@ -72,63 +79,123 @@ sw_daily = np.array([
 ]).T;
 
 # %% Section 4
+# Compute EAPE generation
+# # Compute vertical integral
+def vint(
+    data: np.ndarray,
+    lev : np.ndarray,
+) -> np.ndarray:
+    data_ave: np.ndarray = (data[1:] + data[:-1]) /2.;
+    data_vint: np.ndarray = -np.sum(data_ave * np.diff(lev*100.)[:, None], axis=0) / 9.81 / -np.sum(np.diff(lev*100.));
+    
+    return data_vint;
+
+lw_t_vint: np.ndarray = vint(lw_daily * t_daily, dims["lev"]);
+sw_t_vint: np.ndarray = vint(sw_daily * t_daily, dims["lev"]);
+t_t_vint : np.ndarray = vint(t_daily  * t_daily, dims["lev"]);
+
+lw_eape = 2 * (lw_daily * t_daily) / (t_daily * t_daily);
+sw_eape = 2 * (sw_daily * t_daily) / (t_daily * t_daily);
+
+#lw_eape_vint: np.ndarray = 2*lw_t_vint / t_t_vint;
+#sw_eape_vint: np.ndarray = 2*sw_t_vint / t_t_vint;
+lw_eape_vint: np.ndarray = vint(lw_eape, dims["lev"]);
+sw_eape_vint: np.ndarray = vint(sw_eape, dims["lev"]);
+
+print(lw_eape_vint.sum())
+print(sw_eape_vint.sum())
+
+# %% Section 4
 # Plot
-plt.rcParams["font.family"] = "serif";
-plt.rcParams["mathtext.fontset"] = "cm";
-plt.rcParams["xtick.labelsize"] = 12;
-plt.rcParams["ytick.labelsize"] = 12;
+# Set global font settings
+plt.rcParams["font.family"] = "serif"
+plt.rcParams["mathtext.fontset"] = "cm"
+plt.rcParams["xtick.labelsize"] = 12
+plt.rcParams["ytick.labelsize"] = 12
 
-fig, ax = plt.subplots(2, 1, figsize=(16, 12), sharex=True);
-lw_cf = ax[0].pcolormesh(
+# Create figure and GridSpec layout with increased hspace
+fig = plt.figure(figsize=(16, 15))
+gs = gridspec.GridSpec(4, 1, height_ratios=[1, 0.2, 1, 0.2], hspace=0.35)
+
+# Upper panel (pcolormesh & contour)
+ax0 = fig.add_subplot(gs[0])
+lw_cf = ax0.pcolormesh(
     np.linspace(-2.5, 2.5, 6), dims["lev"],
-    lw_daily,
-    cmap="RdBu_r", norm=TwoSlopeNorm(vcenter=0),
-);
-
-t_cf = ax[0].contour(
+    lw_daily, cmap="RdBu_r", norm=TwoSlopeNorm(vcenter=0),
+)
+t_cf = ax0.contour(
     np.linspace(-3, 2.75, 24), dims["lev"],
-    data_sel["t"],
-    colors="k", levels=np.arange(-2.5, 2.6, 0.5),
-);
-plt.subplots_adjust(left=0.1, bottom=0.1, right=1.03, top=0.92);
-ax[0].set_yscale("log");
-ax[0].set_xticks(np.linspace(-3, 3, 7), np.linspace(-3, 3, 7, dtype=int), fontsize=12);
-ax[0].set_yticks(np.linspace(100, 1000, 10), np.linspace(100, 1000, 10, dtype=int), fontsize=12);
-ax[0].text(3.4, 350, "Level [hPa]", va="center", ha="center", rotation=90, fontsize=16);
-ax[0].text(-4, 350, "LW Heating [K/day]", va="center", ha="center", rotation=90, fontsize=16);
-ax[0].text(3, 95, "(a)\nShading: CNTL LW Heating [K/day]\nContour: CNTL Temperature [K]", va="bottom", ha="left", fontsize=16);
-ax[0].set_xlim(3, -3);
-ax[0].set_ylim(1000, 100);
-plt.colorbar(lw_cf);
-plt.clabel(t_cf, fmt="%1.2f", inline=True, fontsize=10);
+    data_sel["t"], colors="k", levels=np.arange(-2.5, 2.6, 0.5),
+)
+ax0.set_yscale("log")
+ax0.set_yticks(np.linspace(100, 1000, 10), np.linspace(100, 1000, 10, dtype=int))
+ax0.set_xlim(3, -3)
+ax0.set_ylim(1000, 100)
 
-sw_cf = ax[1].pcolormesh(
+ax0.set_title("(a) CNTL LW Heating (Shading) & Temperature (Contour) & EAPE growth rate (Bar)", loc="left", fontsize=16)
+
+# Add colorbar for ax0
+cax0 = inset_axes(ax0, width="1.5%", height="80%", loc="center right",
+                bbox_to_anchor=(0.04, 0, 1, 1),
+                bbox_transform=ax0.transAxes, borderpad=0)
+fig.colorbar(lw_cf, cax=cax0)
+plt.clabel(t_cf, fmt="%1.2f", inline=True, fontsize=10)
+
+# Lower panel (LW bar plot)
+ax1 = fig.add_subplot(gs[1], sharex=ax0)
+lw_positive_cond = np.where(lw_eape_vint > 0)[0]
+lw_negative_cond = np.where(lw_eape_vint < 0)[0]
+ax1.bar(np.linspace(-2.5, 2.5, 6)[lw_positive_cond], lw_eape_vint[lw_positive_cond], color="r", label="LW")
+ax1.bar(np.linspace(-2.5, 2.5, 6)[lw_negative_cond], lw_eape_vint[lw_negative_cond], color="b", label="SW")
+ax1.axhline(0, color="k")
+ax1.set_yticks(np.linspace(-0.5, 0.5, 3))
+ax1.set_xlim(3, -3)
+
+# Align ax1 with ax0
+pos0 = ax0.get_position()
+pos1 = ax1.get_position()
+ax1.set_position([pos1.x0, pos1.y0+0.01, pos0.width, pos1.height])
+
+# Middle panel (SW pcolormesh & contour)
+ax2 = fig.add_subplot(gs[2], sharex=ax0)
+sw_cf = ax2.pcolormesh(
     np.linspace(-2.5, 2.5, 6), dims["lev"],
-    sw_daily,
-    cmap="RdBu_r", norm=TwoSlopeNorm(vcenter=0),
-);
-
-t_cf = ax[1].contour(
+    sw_daily, cmap="RdBu_r", norm=TwoSlopeNorm(vcenter=0),
+)
+t_cf = ax2.contour(
     np.linspace(-3, 2.75, 24), dims["lev"],
-    data_sel["t"],
-    colors="k", levels=np.arange(-2.5, 2.6, 0.5),
-);
-ax[1].set_yscale("log");
-ax[1].set_xticks(np.linspace(-3, 3, 7), np.linspace(-3, 3, 7, dtype=int), fontsize=12);
-ax[1].set_yticks(np.linspace(100, 1000, 10), np.linspace(100, 1000, 10, dtype=int), fontsize=12);
-ax[1].text(3.4, 350, "Level [hPa]", va="center", ha="center", rotation=90, fontsize=16);
-ax[1].text(-4, 350, "SW Heating [K/day]", va="center", ha="center", rotation=90, fontsize=16);
-ax[1].text(1.5, 1300, "Day After", va="center", ha="center", fontsize=16);
-ax[1].text(-1.5, 1300, "Day Before", va="center", ha="center", fontsize=16);
-ax[1].text(3, 95, "(b)\nShading: CNTL SW Heating [K/day]\n"+r"Contour: CNTL $q_v$ [g/kg]", va="bottom", ha="left", fontsize=16);
-ax[1].set_xlim(-3, 3);
-ax[1].set_ylim(100, 1000);
-plt.gca().invert_xaxis();
-plt.gca().invert_yaxis();
-plt.colorbar(sw_cf);
-plt.clabel(t_cf, fmt="%1.2f", inline=True, fontsize=10);
+    data_sel["t"], colors="k", levels=np.arange(-2.5, 2.6, 0.5),
+)
+ax2.set_yscale("log")
+ax2.set_yticks(np.linspace(100, 1000, 10), np.linspace(100, 1000, 10, dtype=int))
 
+ax2.set_ylim(1000, 100)
+ax2.set_title("(b) CNTL SW Heating (Shading) & Temperature (Contour) & EAPE growth rate (Bar)", loc="left", fontsize=16)
+
+# Add colorbar for ax2
+cax2 = inset_axes(ax2, width="1.5%", height="80%", loc="center right",
+                bbox_to_anchor=(0.04, 0, 1, 1),
+                bbox_transform=ax2.transAxes, borderpad=0)
+fig.colorbar(sw_cf, cax=cax2)
+plt.clabel(t_cf, fmt="%1.2f", inline=True, fontsize=10)
+
+# Move ax2 downward (adjusting GridSpec hspace)
+pos2 = ax2.get_position()
+ax2.set_position([pos2.x0, pos2.y0, pos2.width, pos2.height])  # Move down slightly
+
+# Lower panel (SW bar plot)
+ax3 = fig.add_subplot(gs[3], sharex=ax0)
+sw_positive_cond = np.where(sw_eape_vint > 0)[0]
+sw_negative_cond = np.where(sw_eape_vint < 0)[0]
+ax3.bar(np.linspace(-2.5, 2.5, 6)[sw_positive_cond], sw_eape_vint[sw_positive_cond], color="r", label="LW")
+ax3.bar(np.linspace(-2.5, 2.5, 6)[sw_negative_cond], sw_eape_vint[sw_negative_cond], color="b", label="SW")
+ax3.axhline(0, color="k")
+ax3.set_yticks(np.linspace(-0.2, 0.2, 3))
+ax3.set_xlim(3, -3)
+
+# Align ax3 with ax2
+pos2 = ax2.get_position()
+pos3 = ax3.get_position()
+ax3.set_position([pos3.x0, pos3.y0+0.01, pos2.width, pos3.height])
 plt.savefig("/home/b11209013/Bachelor_Thesis/Major/Figure/Figure02.png", dpi=300);
-plt.show();
-
-
+plt.show()
