6e1e047fdcc3ac8dd01f02044ce2c5234ead2f5c
JosephKan828 <josephyck0828@gmail.com>
2025-03-26 14:49:17 +0800

    Major comment

---
+++ CNTL_NCRF/moist_strat.py
@@ -0,0 +1,143 @@
+# This program is to compare the LW heating difference between CNTL and NCRF
+# %% Section 1
+# Import package
+import numpy as np;
+import netCDF4 as nc;
+
+import pickle as pkl;
+from matplotlib import pyplot as plt;
+from matplotlib.colors import TwoSlopeNorm;
+
+# %% Section 2
+# Load data
+# # Load CNTL and NCRF data
+path: str = "/work/b11209013/2024_Research/MPAS/merged_data/"
+
+dims: dict[str, np.ndarray] = dict();
+data: dict[str, np.ndarray] = dict();
+
+with nc.Dataset(f"{path}CNTL/w.nc", "r") as f:
+    for key in f.dimensions.keys():
+        dims[key] = f.variables[key][...];
+
+    lat_lim: tuple[int] = np.where((dims["lat"] >= -5) & (dims["lat"] <= 5))[0];
+    dims["lat"] = dims["lat"][lat_lim];
+
+    data["cntl_w"] = f.variables["w"][:, :, lat_lim, :];
+
+with nc.Dataset(f"{path}CNTL/qv.nc", "r") as f:
+    data["cntl_qv"] = f.variables["qv"][:, :, lat_lim, :] * 1000.;
+
+with nc.Dataset(f"{path}NCRF/w.nc", "r") as f:
+    data["ncrf_w"] = f.variables["w"][:, :, lat_lim, :];
+
+with nc.Dataset(f"{path}NCRF/qv.nc", "r") as f:
+    data["ncrf_qv"] = f.variables["qv"][:, :, lat_lim, :] * 1000.;
+
+# # Load Selected Events
+with open("/home/b11209013/Bachelor_Thesis/Major/CCKWs_Selection/CNTL_comp.pkl", "rb") as f:
+    cntl_comp = pkl.load(f);
+
+with open("/home/b11209013/Bachelor_Thesis/Major/CCKWs_Selection/NCRF_comp.pkl", "rb") as f:
+    ncrf_comp = pkl.load(f);
+
+sel_lon: dict[str, np.ndarray] = dict(
+    cntl=np.array(cntl_comp["sel_lon"]),
+    ncrf=np.array(ncrf_comp["sel_lon"]),
+);
+
+sel_time: dict[str, np.ndarray] = dict(
+    cntl=np.array(cntl_comp["sel_time"]),
+    ncrf=np.array(ncrf_comp["sel_time"]),
+);
+
+# %% Section 3
+# Processing data
+# # Remove climatology and zonal mean
+qv_mean = dict(
+    cntl = np.mean(data["cntl_qv"], axis=(0, 2, 3)),
+    ncrf = np.mean(data["ncrf_qv"], axis=(0, 2, 3)),
+)
+
+w_rm_cli: dict[str, np.ndarray] = dict(
+    cntl = (data["cntl_w"] - np.mean(data["cntl_w"], axis=(0, 2, 3), keepdims=True)).mean(axis=2),
+    ncrf = (data["ncrf_w"] - np.mean(data["ncrf_w"], axis=(0, 2, 3), keepdims=True)).mean(axis=2),
+);
+
+# # Select Events
+data_sel: dict[str, np.ndarray] = dict(
+    cntl_w = np.array([
+        w_rm_cli["cntl"][i-12:i+12, :, j]
+            for i, j in zip(sel_time["cntl"], sel_lon["cntl"])
+    ]).mean(axis=0).T,
+    ncrf_w = np.array([
+        w_rm_cli["ncrf"][i-12:i+12, :, j]
+            for i, j in zip(sel_time["ncrf"], sel_lon["ncrf"])
+    ]).mean(axis=0).T,
+);
+
+# compute vertical gradient in CCKWs
+qv_grad = dict(
+    cntl = np.gradient(qv_mean["cntl"], dims["lev"]*100.),
+    ncrf = np.gradient(qv_mean["ncrf"], dims["lev"]*100.)
+)
+
+vertical_conv = dict(
+    cntl = data_sel["cntl_w"]*qv_grad["cntl"][:, None],
+    ncrf = data_sel["ncrf_w"]*qv_grad["ncrf"][:, None]
+) 
+
+daily_vconv = dict(
+    cntl = np.array([vertical_conv["cntl"][:, i*4:(i+1)*4].mean(axis=1) for i in range(6)]).T,
+    ncrf = np.array([vertical_conv["ncrf"][:, i*4:(i+1)*4].mean(axis=1) for i in range(6)]).T
+)
+plt.rcParams["font.family"]="serif"
+
+fig, ax = plt.subplots(1, 2, figsize=(17, 6), sharey=True)
+
+ax[0].plot(qv_mean["cntl"], dims["lev"], color="blue", label="CNTL")
+ax[0].plot(qv_mean["ncrf"], dims["lev"], color="red", label="NCRF")
+ax[0].set_xlim(0, 17)
+ax[0].set_ylim(1000, 400)
+ax[0].set_yscale("log")
+ax[0].set_yticks(np.linspace(1000, 400, 7), np.linspace(1000, 400, 7, dtype=int))
+ax[0].set_xlabel(r"$q_v$ [g/kg]", fontsize=14)
+ax[0].set_ylabel(r"Level [hPa]", fontsize=14)
+ax[0].set_title("(a) Mean (x, y, t) Moisture Profile of CNTL(b) and NCRF(r)", fontsize=14)
+ax[0].legend()
+
+ax[1].plot(qv_grad["cntl"]*1e4, dims["lev"], color="blue", label="CNTL")
+ax[1].plot(qv_grad["ncrf"]*1e4, dims["lev"], color="red", label="NCRF")
+ax[1].set_xlim(0, 5)
+#plt.xlim(10, 17)
+#ax[1].set_yscale("log")
+#ax[1].set_yticks(np.linspace(800, 1000, 11), np.linspace(800, 1000, 11, dtype=int))
+#ax[1].set_ylim(1000, 400)
+ax[1].set_xlabel(r"$\frac{\partial q_v}{\partial z}$ [g/kg]", fontsize=14)
+#ax[1].set_ylabel(r"Level [hPa]", fontsize=14)
+ax[1].set_title("(b) Vertical Gradient of Moisture Profile of CNTL(b) and NCRF(r)", fontsize=14)
+ax[1].legend()
+#plt.savefig("/home/b11209013/Bachelor_Thesis/Major/Figure/Appendix04.png", dpi=300)
+plt.show()
+
+
+def vert_int(data):
+    data_ave = (data[1:] + data[:-1]) / 2.;
+    data_vint = -np.sum(data_ave * np.diff(dims["lev"]*100.)[:, None], axis=0) / -np.sum(np.diff(dims["lev"]*100.));
+    return data_vint;
+
+diff = vert_int(daily_vconv["ncrf"]) - vert_int(daily_vconv["cntl"])
+pos = np.where(diff >= 0)
+neg = np.where(diff <0)
+
+plt.bar(np.linspace(-2.5, 2.5, 6)[pos], diff[pos], color="red", width=0.2)
+plt.bar(np.linspace(-2.5, 2.5, 6)[neg], diff[neg], color="blue", width=0.2)
+plt.axhline(0, linestyle="--", color="black")
+plt.xlim(-3, 3)
+plt.ylim(-6e-7, 6e-7)
+plt.xlabel("Lag days")
+plt.ylabel("Vertical Moisture Convergence")
+# plt.savefig("/home/b11209013/Bachelor_Thesis/Major/Figure/Appendix05.png", dpi=300)
+plt.show()
+
+print((vert_int(daily_vconv["ncrf"]) - vert_int(daily_vconv["cntl"])).sum())
---
+++ CNTL_NSC/q_var.py
@@ -0,0 +1,135 @@
+# This program is to compare the LW heating difference between CNTL and NCRF
+# %% Section 1
+# Import package
+import numpy as np;
+import xarray as xr;
+
+from scipy import fft;
+from matplotlib import pyplot as plt;
+from matplotlib.colors import TwoSlopeNorm;
+
+# functions
+def kel_filter(data, axes=(None, None)):
+    data_shape: tuple(int) = data.shape;
+
+    wn: np.ndarray[int]   = fft.fftfreq(data_shape[axes[0]], d=1/data_shape[axes[0]]).astype(int);
+    fr: np.ndarray[float] = fft.fftfreq(data_shape[axes[1]], d=1/4);
+
+    wnm, frm = np.meshgrid(wn, fr);
+
+    kel_curve = lambda wn, eq: wn * np.sqrt(9.81*eq) * 86400 / (2*np.pi*6.371e6);
+
+    pos_cond = (
+        (wnm >= 1) & (wnm <= 14) &
+        (frm >= 1/20) & (frm <= 1/2.5) &
+        (frm >= kel_curve(wnm, 8)) & (frm <= kel_curve(wnm, 90))
+    );
+
+    neg_cond = (
+        (wnm <= -1) & (wnm >= -14) &
+        (frm <= -1/20) & (frm >= -1/2.5) &
+        (frm <= kel_curve(wnm, 8)) & (frm >= kel_curve(wnm, 90))
+    );
+
+    kel_mask: np.ndarray  = np.where(pos_cond | neg_cond, 1, 0);
+
+    data_fft = fft.fft(data, axis=axes[0]);
+    data_fft = fft.ifft(data_fft, axis=axes[1]) * data.shape[axes[1]];
+
+    data_mask: np.ndarray = data_fft * kel_mask[:, None, :];
+
+    return wn, fr, data_mask;
+
+def reconstruct(data, axes=(None, None)):
+
+    data_shape: tuple(int) = data.shape;
+
+    data_ifft = fft.ifft(data, axis=axes[0]);
+    data_ifft = fft.fft(data_ifft, axis=axes[1]) / data.shape[axes[1]];
+
+    return data_ifft;
+
+# vertical integral
+def vint(data, lev, init_lev, term_lev, axis=1):
+    lev_rng = np.where((lev >= init_lev) & (lev <= term_lev))[0];
+
+    lev_sel = lev[lev_rng];
+    data_sel = data[lev_rng];
+
+    data_ave = (data_sel[1:] + data_sel[:-1]) / 2.;
+    data_vint = np.nansum(data_ave * lev_sel.diff(dim="lev")) / np.nansum(lev_sel.diff(dim="lev"));
+
+    return data_vint;
+
+# %% Section 2
+# Load data
+# # Load CNTL and NCRF data
+path: str = "/work/b11209013/2024_Research/MPAS/merged_data/"
+
+dims: dict[str, np.ndarray] = dict();
+data: dict[str, np.ndarray] = dict();
+
+with xr.open_dataset(f"{path}CNTL/qv.nc") as f:
+    f = f.sel(lat=slice(-5, 5));
+
+    cntl_dims = f.coords;
+
+    data["cntl"] = f["qv"] * 1000.;
+
+with xr.open_dataset(f"{path}NSC/qv.nc") as f:
+    f = f.sel(lat=slice(-5, 5));
+
+    ncrf_dims = f.coords;
+
+    data["ncrf"] = f["qv"] * 1000.;
+
+# Acquire anomalous data (rm cli and zonal)
+data_ano : dict[str, np.ndarray] = dict(
+    (key, data[key] - data[key].mean(dim={"time", "lon"}))
+    for key in data.keys()
+);
+
+del data;
+
+# Symmetric data
+data_sym: dict[str, np.ndarray] = dict(
+    (key, ((data_ano[key] + data_ano[key][:, :, :, ::-1])/2).mean(dim={"lat"}))
+    for key in data_ano.keys()
+);
+
+# Bandpass filter
+cntl_filt = kel_filter(data_sym["cntl"], axes=(-1, 0));
+ncrf_filt = kel_filter(data_sym["ncrf"], axes=(-1, 0));
+
+wn: dict[str, np.ndarray] = dict(
+    cntl = cntl_filt[0],
+    ncrf = ncrf_filt[0],
+);
+
+fr: dict[str, np.ndarray] = dict(
+    cntl = cntl_filt[1],
+    ncrf = ncrf_filt[1],
+);
+
+data_masked: dict[str, np.ndarray] = dict(
+    cntl = cntl_filt[2],
+    ncrf = ncrf_filt[2],
+);
+
+del cntl_filt, ncrf_filt;
+
+data_recon: dict[str, np.ndarray] = dict(
+    (key, reconstruct(data_masked[key], axes=(-1, 0)))
+    for key in data_masked.keys()
+);
+
+print(data_recon["cntl"].shape);
+
+# vertically integrate over specific levels
+data_vint_500: dict[str, np.ndarray] = dict(
+    cntl = vint(np.var(data_recon["cntl"], axis=(0, -1)), cntl_dims["lev"]*100., 30000, 70000),
+    ncrf = vint(np.var(data_recon["ncrf"], axis=(0, -1)), ncrf_dims["lev"]*100., 30000, 70000),
+);
+print("vertically integrate over 300~500 hPa:");
+print(data_vint_500);
+
